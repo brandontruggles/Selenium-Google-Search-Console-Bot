b3ad68a9a8339129f92db47fa2e90d22b0ccc744
Brandon Ruggles <brandonrninefive@gmail.com>
2017-03-19 02:44:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Selenium Google Search Console Bot
+This is a simple Python bot which uses Selenium to scrape the Google Search Console Dashboard [https://www.google.com/webmasters/tools/home](https://www.google.com/webmasters/tools/home)  for the number of total indexed urls for a particular site. This should be possible with a simple API call, however, Google has not yet implemented an API call for this particular number. This script also scrapes other data available from the dashboard including the total number of *soft 404* errors, the total number of *not found* errors, and the total of *other* errors for a particular site. However, these numbers **can** be obtained through the use of the [Google Search Console API](https://developers.google.com/webmaster-tools/search-console-api-original/v3/urlcrawlerrorscounts/query). After scraping the data, the script then writes the data to a Google Sheet using the [Google Sheets API](https://developers.google.com/sheets/). The script is configured to use Chromedriver, which is included as a Pip dependency, but PhantomJS is included as a NodeJS dependency in the case that you want to use a headless webdriver.
+
+## Installation Instructions (Linux/Mac)
+
+1. Make sure that you have Python 2.7.X installed, along with Pip.
+
+2. Install Node.JS and NPM.
+
+3. Download or clone this repository to a directory on your machine.
+
+4. Add the directory containing this project into your global PYTHONPATH environment variable. *NOTE: If you get an error stating that checkout.conf cannot be found, or that it contains invalid JSON, you may not have performed this step.*
+
+5. Open the terminal, navigate to the project directory, and type the command `pip install -r requirements.txt`. This should install the Python dependencies Selenium and Chromedriver. *NOTE: On Mac, the dependency chromedriver-installer may not download. If this is the case, you will have to install Chromedriver manually.*
+
+6. From the terminal, type the command `npm install`. This should install PhantomJS.
+
+7. Edit the scrape.conf JSON file to your liking, but make sure that it still contains valid JSON syntax. This file allows you to control which website on your dashboard will be analyzed by the script, the ID of the Google Spreadsheet to write output to, the ID of the Google Sheet within the Google Spreadsheet to write output to, the coordinates of where the output row should be written on the Google Sheet, as well as the order that the output is written. The order and size of the output is determined by the "output" key in `scrape.conf`, which uses the following variables to represent different numbers:
+
+* `date` - The current month and current year.
+
+* `soft404Num` - The number of "soft 404" errors for the current month.
+
+* `notFoundNum` - The number of "not found" errors for the current month.
+
+* `otherNum` - The number of "other" errors for the current month.
+
+* `indexNum` - The total number of pages indexed.
+
+8. You may edit autocheckout.py to use PhantomJS instead of Chromedriver if you do not wish to view the browser navigation performed by the script.
+
+9. In order to access the Google Sheets API with this script, you must follow the insructions for creating an API project and downloading a `client_secret.json` file to the root project directory. These instructions are available here: [https://developers.google.com/api-client-library/python/auth/installed-app#creatingcred](https://developers.google.com/api-client-library/python/auth/installed-app#creatingcred). 
+
+10. Run the program from the terminal with `python scrapedata.py`. **NOTE:** There are a series of flags that can be used when executing the script which will change its behavior. The following are the most useful of these flags:
+
+* `--noauth_local_webserver` - Displays an authentication link in the terminal rather than opening an authentication page in a browser window. Allows you to authenticate while logged into a server remotely.
+
+* `--refresh` - The script will not perform the data scraping or Google Sheets uploading processes, and will instead simply refresh the OAuth token and redownload the cookies from the browser after logging in. This is useful for keeping login information persistent over long periods of time with no user intervention.
+
+11. If it is your first time running this script, or your login tokens have expired since your last run, you will be prompted for your Google credentials upon running the script. To securely avoid having to ask for these credentials upon every run, the script will save your Selenium browser cookies each time you run the script. These cookies are saved to the local file `cookies.dat`, where they are read for subsequent runs.
+
+## Installation Instructions (Windows)
+
+This script has not been tested for Windows, but it should still work if a similar procedure is followed as the one for Linux/Mac.
---
+++ scrapedata.py
@@ -0,0 +1,200 @@
+#################################################################################################################################################################
+#Written by Brandon Ruggles (brandonrninefive@gmail.com) :)
+##################################################################################################################################################################
+
+import json
+import time
+import datetime
+import getpass
+import pickle
+
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+	import argparse
+	parser = argparse.ArgumentParser(parents=[tools.argparser])
+	parser.add_argument("--refresh", help="Only refresh the cookies and oAuth tokens, do NOT parse any data or upload any data to Google Sheets.", action="store_true")
+	flags = parser.parse_args()
+except ImportError:
+	flags = None
+
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+confFile = None
+cookieFile = None
+cookieData = None
+username = ""
+password = ""
+debug = True
+
+try:
+	#We read our JSON data from a file called 'scrape.conf' for simplicity and readability.
+	confFile = open("scrape.conf")
+	confJSON = json.loads(confFile.read())
+	site = confJSON["site"]
+	spreadsheetId = confJSON["spreadsheetId"]
+	sheetId = confJSON["sheetId"]
+	rowIndex = confJSON["rowIndex"]
+	columnIndex = confJSON["columnIndex"]
+	confOutput = confJSON["output"]
+except Exception as e:
+	print "Error: either no scrape.conf was found, or it contains invalid JSON syntax!"
+	if(debug):
+		print "Error Details: " + str(e)
+try:
+	#We read our cookies from a file called 'cookies.dat' (used for storing Google login credentials securely.
+	cookieFile = open("cookies.dat")
+	cookieData = pickle.loads(cookieFile.read())
+except Exception as e:
+	print "Error: either no cookies.dat was found, or the data it contains could not be read!"
+	if(debug):
+		print "Error Details: " + str(e)
+
+	username = raw_input("Please type in your Google username: ")
+	password = getpass.getpass(prompt="Please type in your Google password: ")
+
+def login(username, password, cookieData):
+	if(cookieData != None):
+		driver.get('https://www.google.com/')
+		for cookie in cookieData:
+			driver.add_cookie(cookie)
+		driver.get('https://www.google.com/webmasters/tools/home')
+	else:
+		driver.get('https://www.google.com/webmasters/tools/home')
+		boxDiv = driver.find_element_by_xpath('/html/body/div/div/div/div/form/div/div/div/div/div')
+		usernameBox = boxDiv.find_element_by_xpath('./input[@id="Email"]')
+
+		usernameBox.send_keys(username)
+		usernameBox.send_keys(Keys.ENTER)
+		boxDiv = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH,'/html/body/div/div[2]/div[2]/div[1]/form/div[2]/div/div[2]/div/div')))
+		passwordBox = boxDiv.find_element_by_xpath('./input[@id="Passwd"]')
+
+		passwordBox.send_keys(password)
+		passwordBox.send_keys(Keys.ENTER)
+	
+def scrape():
+	print "Attempting to scrape the Google Search Console data..."
+	siteLink = None
+	siteLinks = WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.XPATH, '/html/body/div[1]/div[3]/div[2]/div/div/div/div/div/table/tbody/tr/td[2]/table/thead/tr/th[2]/div[1]/a')))
+	for link in siteLinks:
+		if(link.text == site):
+			siteLink = link
+			break
+	siteLink.click()
+	soft404Div = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="wmx_gwt_feature_DASHBOARD"]/div/div[5]/div[1]/div/table/tbody/tr[1]/td/div/div/div[2]/div[2]/table/tbody/tr/td[2]/div/div[1]')))
+	notFoundDiv = driver.find_element_by_xpath('//*[@id="wmx_gwt_feature_DASHBOARD"]/div/div[5]/div[1]/div/table/tbody/tr[1]/td/div/div/div[2]/div[2]/table/tbody/tr/td[2]/div/div[2]')
+	otherDiv = driver.find_element_by_xpath('//*[@id="wmx_gwt_feature_DASHBOARD"]/div/div[5]/div[1]/div/table/tbody/tr[1]/td/div/div/div[2]/div[2]/table/tbody/tr/td[1]/div/div[3]')
+	soft404Num = long(soft404Div.find_element_by_xpath('./b').get_attribute('innerHTML').replace(",", ""))
+	notFoundNum = long(notFoundDiv.find_element_by_xpath('./b').get_attribute('innerHTML').replace(",", ""))
+	otherNum = long(otherDiv.find_element_by_xpath('./b').get_attribute('innerHTML').replace(",", ""))
+	month = str(datetime.date.today().month)
+	year = str(datetime.date.today().year)
+	if(int(month) < 10):
+		month = "0" + month
+	date = year + "|" + month
+	indexDiv = driver.find_element_by_xpath('//*[@id="navigation"]/li[5]/div')
+	indexDiv.click()
+	indexStatusButton = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="navigation"]/li[5]/ul/li[1]/a')))
+	indexStatusButton.click()
+	indexDiv = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="index-status-chart-legend"]/div/table/tbody/tr[2]/td/div')))
+	indexNum = long(indexDiv.get_attribute('innerHTML').replace(",", ""))
+
+	#Change the order of these values to change the order that they are pasted into the spreadsheet
+	outputArr = eval(confOutput)
+	print "Successfully scraped all of the requested data!"
+	return outputArr
+
+def getDictForItem(item):
+	returnDict = {}
+	if(isinstance(item, int) or isinstance(item, float) or isinstance(item, long) or isinstance(item, complex)):
+		returnDict = {"numberValue": item}
+	elif(isinstance(item, bool)):
+		returnDict = {"boolValue": item}
+	elif(isinstance(item, basestring)):
+		returnDict = {"stringValue": item}
+	else:
+		returnDict = {"stringValue": ""}
+		print "The type of the item was not compatible with the Sheets API! Item: " + str(item)
+	return returnDict
+
+def getAPICredentials():
+	print "Attempting to retrieve API credentials..."
+	SCOPES = "https://www.googleapis.com/auth/spreadsheets"
+	CLIENT_SECRET_FILE = "client_secret.json"	
+	CREDENTIAL_FILE = "credentials.json"
+	APPLICATION_NAME = "Selenium Google Search Console Bot"	
+	store = Storage(CREDENTIAL_FILE)
+	credentials = store.get()
+
+	if(not credentials or credentials.invalid):
+		flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+		flow.user_agent = APPLICATION_NAME
+		if(flags):
+			credentials = tools.run_flow(flow, store, flags)
+		else:
+			credentials = tools.run(flow, store)
+	print "The API credentials were retrieved successfully!"
+	return credentials
+
+def writeToCSV(outputArr):
+	print 'Attempting to write data to the specified Google Sheet...'
+	
+	http = getAPICredentials().authorize(httplib2.Http())
+	discoveryUrl = ("https://sheets.googleapis.com/$discovery/rest?version=v4")
+	service = discovery.build("sheets", "v4", http=http, discoveryServiceUrl=discoveryUrl)
+	valuesToWrite = []
+	for item in outputArr:
+		valuesToWrite.append({"userEnteredValue": getDictForItem(item)})
+	update_request_body = {
+		"requests": [
+			{
+				"updateCells": {
+					"rows": [{"values": valuesToWrite}],
+					"fields": "*",
+					"start": {"sheetId": sheetId, "rowIndex": rowIndex, "columnIndex": columnIndex}
+				}
+			}
+		]
+	}
+	
+	result = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body=update_request_body).execute()
+	print "Response to update request: " + str(result)
+
+startTime = time.time()
+
+driver = webdriver.Chrome() #Non-headless webdriver
+#driver = webdriver.PhantomJS() #Headless webdriver
+try:
+	login(username, password, cookieData)
+	if(not flags.refresh):
+		writeToCSV(scrape())
+	else:
+		getAPICredentials()
+	print "Writing cookie data to 'cookies.dat'..." 
+	dump = pickle.dumps(driver.get_cookies())
+	cookieFile = open("cookies.dat", "w")
+	cookieFile.write(dump)
+	cookieFile.close()
+	print "Successfully wrote cookies to 'cookies.dat'!"
+	driver.close()
+	if(flags.refresh):
+		print "The script has finished refreshing the cookies and oAuth tokens successfully!"
+	else:
+		print "The script has successfully finished executing!"
+except Exception as e:
+	print "An error occurred!"
+	if(debug):
+		print "Error Details: " + str(e) 
+
+print "Total elapsed script time: " + str(time.time() - startTime) + " second(s)."
